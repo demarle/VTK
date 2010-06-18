demarle/VTK

commit f4f2f731055ca02036e7570f46f0d951af07424e

Use Get*Buffer() instead of GL_FRONT/GL_BACK in a few remaining places.

Rendering/vtkOpenGLRenderWindow.cxx
@@ -617,11 +617,11 @@ int vtkOpenGLRenderWindow::SetPixelData(int x1, int y1, int x2, int y2,
 
   if (front)
     {
-    glDrawBuffer(GL_FRONT);
+    glDrawBuffer(this->GetFrontBuffer());
     }
   else
     {
-    glDrawBuffer(GL_BACK);
+    glDrawBuffer(this->GetBackBuffer());
     }
 
   if (y1 < y2)
@@ -962,11 +962,11 @@ int vtkOpenGLRenderWindow::SetRGBAPixelData(int x1, int y1, int x2, int y2,
 
   if (front)
     {
-    glDrawBuffer(GL_FRONT);
+    glDrawBuffer(this->GetFrontBuffer());
     }
   else
     {
-    glDrawBuffer(GL_BACK);
+    glDrawBuffer(this->GetBackBuffer());
     }
 
   if (y1 < y2)
@@ -1271,11 +1271,11 @@ int vtkOpenGLRenderWindow::SetRGBACharPixelData(int x1, int y1, int x2,
 
   if (front)
     {
-    glDrawBuffer(GL_FRONT);
+    glDrawBuffer(this->GetFrontBuffer());
     }
   else
     {
-    glDrawBuffer(GL_BACK);
+    glDrawBuffer(this->GetFrontBuffer());
     }
 
 

commit d7dac31f3009f0f842132816e8bee5b19650af66

Fix to use back buffer correctly.
Fixes a regression in commit f4f2f731055ca02036e7570f46f0d951af07424e.

Rendering/vtkOpenGLRenderWindow.cxx
@@ -1275,7 +1275,7 @@ int vtkOpenGLRenderWindow::SetRGBACharPixelData(int x1, int y1, int x2,
     }
   else
     {
-    glDrawBuffer(this->GetFrontBuffer());
+    glDrawBuffer(this->GetBackBuffer());
     }
 
 