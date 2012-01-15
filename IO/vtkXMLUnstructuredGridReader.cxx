demarle/VTK

commit 4f54be9b7b4481235422d50e7605dc1c48e98713

Fixed BUG #12202. vtkXMLUnstructuredGridReader memory blot.
vtkXMLUnstructuredGridReader kept on increasing memory size as one when over
timesteps for a temporal dataset. The issue was that the reader was
miscalculating the offset into the cellarray for the active piece. That offset
kept on increasing for every timestep. Fixed that.

Also fixed vtkXMLDataReader to mark array modified after reading so that they
realize that the internal data has been changed.

Change-Id: I0e6f196d3da41af1ec0337cf86cafa55e35e8635

IO/vtkXMLUnstructuredGridReader.cxx
@@ -248,14 +248,6 @@ int vtkXMLUnstructuredGridReader::ReadPieceData()
   vtkUnstructuredGrid* output = vtkUnstructuredGrid::SafeDownCast(
       this->GetCurrentOutput());
 
-  // Save the start location where the new cell connectivity will be
-  // appended.
-  vtkIdType startLoc = 0;
-  if(output->GetCells()->GetData())
-    {
-    startLoc = output->GetCells()->GetData()->GetNumberOfTuples();
-    }
-
   // Set the range of progress for the cell specifications.
   this->SetProgressRange(progressRange, 1, fractions);
 
@@ -284,6 +276,15 @@ int vtkXMLUnstructuredGridReader::ReadPieceData()
   // Construct the cell locations.
   vtkIdTypeArray* locations = output->GetCellLocationsArray();
   vtkIdType* locs = locations->GetPointer(this->StartCell);
+  vtkIdTypeArray* cellArrayData = output->GetCells()->GetData();
+  vtkIdType startLoc = 0;
+  if (this->StartCell > 0)
+    {
+    // this set the startLoc to point to the location in the cellArray where the
+    // cell for this piece will start writing.
+    startLoc = locations->GetValue(this->StartCell-1) +
+      cellArrayData->GetValue(locations->GetValue(this->StartCell-1));
+    }
   vtkIdType* begin = output->GetCells()->GetData()->GetPointer(startLoc);
   vtkIdType* cur = begin;
   vtkIdType i;

IO/vtkXMLDataReader.cxx
@@ -774,6 +774,14 @@ int vtkXMLDataReader::ReadArrayValues(vtkXMLDataElement* da, vtkIdType arrayInde
     {
     iter->Delete();
     }
+  // Marking the array modified is essential, since otherwise, when reading
+  // multiple time-steps, the array does not realize that its contents may have
+  // changed and does not recompute the array ranges.
+  // This becomes an issue only because we reuse the vtkAbstractArray* instance
+  // when reading time-steps. The array is allocated only for the first timestep
+  // read (see vtkXMLReader::ReadXMLData() and its use of
+  // this->TimeStepWasReadOnce flag).
+  array->Modified();
   this->InReadData = 0;
   return result;
 }

commit 2741a00ca8677d79c29d208d92c1f7f2a3bd1309

Fixed BUG #12788. Segfault when reading files with multiple pieces.
The bug was introduced by 4f54be9b. The computation of the locations offset was
incorrect (we were missing taking into account the location for placing the
count for items in the cell). Fixed that.

IO/vtkXMLUnstructuredGridReader.cxx
@@ -282,8 +282,14 @@ int vtkXMLUnstructuredGridReader::ReadPieceData()
     {
     // this set the startLoc to point to the location in the cellArray where the
     // cell for this piece will start writing.
-    startLoc = locations->GetValue(this->StartCell-1) +
-      cellArrayData->GetValue(locations->GetValue(this->StartCell-1));
+
+    // Id for last written cell:
+    vtkIdType lastWrittenCell = this->StartCell - 1;
+    vtkIdType locationOfLastWrittenCell = locations->GetValue(lastWrittenCell);
+    startLoc = locationOfLastWrittenCell + 1 +
+      cellArrayData->GetValue(locationOfLastWrittenCell);
+    // startLoc = location-of-last-written-cell + 1 (for put the count for items in the cell)
+    //            + (number of items in the cell).
     }
   vtkIdType* begin = output->GetCells()->GetData()->GetPointer(startLoc);
   vtkIdType* cur = begin;