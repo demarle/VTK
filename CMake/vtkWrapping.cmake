demarle/VTK

commit b26e66f91c9f6b1e861cbb3111cafa9484ce8739

Add support for selectively Python wrapping modules
Introduce VTK_WRAP_PYTHON_MODULES variable that can
be used to explicitly control which modules get
Python wrapped. At this time no automatic dependency
support is provided, however, when the concept of
"private dependencies" is introduced this can be
addressed.

Change-Id: Ia568a355e41c39bb026ddc649797c06cfa94e27b

CMake/vtkWrapping.cmake
@@ -58,7 +58,9 @@ function(vtk_add_wrapping module_name module_srcs module_hdrs)
 
       # Now to wrap the languages that are on.
       if(VTK_WRAP_PYTHON)
-        vtk_add_python_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")
+        if (${module_name}_WRAP_PYTHON)
+          vtk_add_python_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")
+        endif()
       endif()
       if(VTK_WRAP_TCL)
         vtk_add_tcl_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")

CMake/vtkModuleTop.cmake
@@ -236,6 +236,22 @@ macro(init_module_vars)
   set(${vtk-module}-targets-build "${VTK_BINARY_DIR}/VTKTargets.cmake")
 endmacro()
 
+# VTK_WRAP_PYTHON_MODULES can be used to explicitly control which modules
+# get Python wrapped (no automatic dependency support is provided at this
+# time). If it has been set mark the modules in the list as such.
+# Note any wrap exclude entries in the module.cmake will take precedence
+# If entry has not been set default to VTK_MODULES_ENABLED.
+if(VTK_WRAP_PYTHON)
+  if(NOT VTK_WRAP_PYTHON_MODULES)
+    set(VTK_WRAP_PYTHON_MODULES ${VTK_MODULES_ENABLED})
+  endif()
+  foreach(_wrap_module ${VTK_WRAP_PYTHON_MODULES})
+    if(NOT ${_wrap_module}_EXCLUDE_FROM_WRAPPING)
+      set(${_wrap_module}_WRAP_PYTHON ON)
+    endif()
+  endforeach()
+endif()
+
 # Build all modules.
 foreach(vtk-module ${VTK_MODULES_ENABLED})
 

CMake/vtkPythonWrapping.cmake
@@ -30,7 +30,7 @@ function(vtk_add_python_wrapping module_name module_srcs module_hdrs)
   # Figure out the dependent PythonXYD libraries for the module
   foreach(dep ${${vtk-module}_LINK_DEPENDS})
     if(NOT "${vtk-module}" STREQUAL "${dep}")
-      if(NOT ${dep}_EXCLUDE_FROM_WRAPPING)
+      if(${dep}_WRAP_PYTHON)
         list(APPEND extra_links ${dep}PythonD)
         list(APPEND VTK_WRAP_INCLUDE_DIRS ${${dep}_SOURCE_DIR})
       endif()

commit 60e9626c556f7fa8a5e40f33f78ebc96a0bc7358

Set VTK_PYTHON_WRAPPED correctly.
Commit b26e66f9 introduced a bug where VTK_PYTHON_WRAPPED global property wasn't
being set correctly. It included modules that weren't being Python wrapped due
to the newly added variables. This caused static builds to fail miserably.
Fixing the logic and also cleanin up the setting of those global properties
closer to the code that actually does the wrapping to avoid such oversight  in
future.

Change-Id: I9884820680f496f2e22ddb287f345e7ff606451e

CMake/vtkWrapping.cmake
@@ -24,18 +24,10 @@ endif()
 function(vtk_add_wrapping module_name module_srcs module_hdrs)
   if(NOT ${module_name}_EXCLUDE_FROM_WRAPPING)
     set(_wrap_module FALSE)
-    if(VTK_WRAP_PYTHON)
+    if(VTK_WRAP_PYTHON OR VTK_WRAP_TCL OR VTK_WRAP_JAVA)
       set(_wrap_module TRUE)
-      set_property(GLOBAL APPEND PROPERTY VTK_PYTHON_WRAPPED ${module_name})
     endif()
-    if(VTK_WRAP_TCL)
-      set(_wrap_module TRUE)
-      set_property(GLOBAL APPEND PROPERTY VTK_TCL_WRAPPED ${module_name})
-    endif()
-    if(VTK_WRAP_JAVA)
-      set(_wrap_module TRUE)
-      set_property(GLOBAL APPEND PROPERTY VTK_JAVA_WRAPPED ${module_name})
-    endif()
+
     if(_wrap_module)
       # The list of include dirs to pass to wrapper tool command lines
       set(VTK_WRAP_INCLUDE_DIRS)
@@ -59,13 +51,16 @@ function(vtk_add_wrapping module_name module_srcs module_hdrs)
       # Now to wrap the languages that are on.
       if(VTK_WRAP_PYTHON)
         if (${module_name}_WRAP_PYTHON)
+          set_property(GLOBAL APPEND PROPERTY VTK_PYTHON_WRAPPED ${module_name})
           vtk_add_python_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")
         endif()
       endif()
       if(VTK_WRAP_TCL)
+        set_property(GLOBAL APPEND PROPERTY VTK_TCL_WRAPPED ${module_name})
         vtk_add_tcl_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")
       endif()
       if(VTK_WRAP_JAVA)
+        set_property(GLOBAL APPEND PROPERTY VTK_JAVA_WRAPPED ${module_name})
         vtk_add_java_wrapping(${module_name} "${module_srcs}" "${module_hdrs}")
       endif()
     endif()