0d7b70006dd1a643d6478977c457edf333b65df3
Dave DeMarle <dave.demarle@kitware.com>
2015-09-14 16:00:14 -0400

    stub out tests that need to pass before we open up to all of VTK's tests

---
+++ Rendering/Ospray/Testing/Cxx/TestOsprayLayers.cxx
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    TestOsprayLayers.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// This test verifies that
+
+// The command line arguments are:
+// -I        => run in interactive mode; unless this is used, the program will
+//              not allow interaction and exit
+
+#include "vtkTestUtilities.h"
+
+int TestOsprayLayers(int argc, char* argv[])
+{
+  int retVal = 1;
+  return !retVal;
+}
